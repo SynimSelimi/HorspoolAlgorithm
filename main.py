93be29e74f27dbfb758d2e083e0040f46e158afa
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2020-12-26 14:28:31 +0100

    Implement base horspool searching algorithm

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ def shift_table(pattern):
     sh_table[pattern[i]] = pattern_len - i 
   return sh_table
 
+def horspool_search(text, pattern): 
+  sh_table = shift_table(pattern)
+  ptrn_len = len(pattern)
+  i = len(pattern) - 1
+
+  while i <= len(text) - 1:
+    matches = 0
+    is_a_match = pattern[ptrn_len - 1 - matches] == text[i - matches]
+    while matches < ptrn_len and is_a_match:
+      is_a_match = pattern[ptrn_len - 1 - matches] == text[i - matches]
+      matches += 1
+    if matches == ptrn_len:
+      return i - matches + 1
+    else:
+      offset = sh_table.get(text[i]) or ptrn_len
+      i += offset
+  return -1
+
 def main():
   print("[+] Horspool Algorithm Demo")
   
@@ -13,12 +31,12 @@ def main():
   
   print(f'\n[+] Searching for "{pattern}" in message...')
   
-  # position = horspool_search(text, pattern)
-  position = -1
-  sh_table = shift_table(pattern)
-  print(sh_table)
+  position = horspool_search(text, pattern)
 
   print((f'Found at {position}' if position != -1 else "Not Found"))
+  if position != -1:
+    ptrn_len = len(pattern)
+    print(f'...{text[position - ptrn_len - 10:position + ptrn_len + 10]}...')
 
 if __name__ == "__main__":
   main()
