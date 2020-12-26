9534b571b273424c0bf778bcbdbd8a79e51c62a9
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2020-12-26 14:06:04 +0100

    Add the shift table method and a shift test case

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def shift_table(pattern): 
+  sh_table = dict()
+  pattern_len = len(pattern) - 1
+  for i in range(0, pattern_len): 
+    sh_table[pattern[i]] = pattern_len - i 
+  return sh_table
+
 def main():
   print("[+] Horspool Algorithm Demo")
   
@@ -8,6 +15,8 @@ def main():
   
   # position = horspool_search(text, pattern)
   position = -1
+  sh_table = shift_table(pattern)
+  print(sh_table)
 
   print((f'Found at {position}' if position != -1 else "Not Found"))
 
